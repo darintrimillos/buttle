12b44d37da79177240a54cb6fa20486cc9cc3f25
Darin Trimillos <darin@trimillos.com>
2021-11-26 18:56:18 -0800

    restructure folders

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,12 +1,19 @@
 import recipes from '../data/recipes/recipes';
 
-const Menu = () => {
+const Menu = ({ recipes }) => {
     console.log('recipes', recipes);
+    console.log('test');
     return (
         <div>
             <h1>The Menu</h1>
         </div>
     )
+}
+
+export async function getStaticProps(context) {
+    return {
+        props: { recipes }
+    }
 }
 
 export default Menu;
