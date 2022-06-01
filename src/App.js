a4afacd9a0bf72882d385d56a0656679acaaf2a5
Darin Trimillos <darin@trimillos.com>
2022-05-31 22:40:16 -0700

    add sidebar links, styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import styles from 'css/App.module.scss';
-import { RecipeBox, Menu } from 'pages/';
+import { Menu, RecipeBox } from 'pages/';
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link
-  } from 'react-router-dom';
+    BrowserRouter as Router, Link, Route, Routes
+} from 'react-router-dom';
 
 function App() {
     return (
         <div className={styles.wrapper}>
             <Router>
                 <aside className={styles.sidebar}>
+                    <h1>The<br />House<br />Menu</h1>
                     <ul>
                         <li><Link to="/">Recipe Box</Link></li>
                         <li><Link to='menu'>Menu</Link></li>
