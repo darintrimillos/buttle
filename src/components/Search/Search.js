bfb5ed6c4bc203dd3fcf3daccf67af70d7cc5735
darintrimillos <darin@trimillos.com>
2023-05-05 23:22:26 -0700

    update <Search />

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,14 @@
-const Search = () => {
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
+import styles from './Search.module.scss';
+
+const Search = ({className, ...rest}) => {
     return (
-        <input type="text" placeholder="Search" />
+        <div className={clsx(styles.search, className && className)} {...rest}>
+            <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+            <input className={styles.input} placeholder="Search" type="text" />
+        </div>
     )
 };
 
